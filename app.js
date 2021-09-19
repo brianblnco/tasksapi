eabb61a33b07b1938ab41c5d4640f7be98a71a5b
Brian <brian@brian.com>
2021-09-20 00:36:11 +0200

    v1.1

--- app.js
+++ app.js
@@ -14,9 +14,47 @@ document.getElementById("formTask").addEventListener('submit', saveTask);
             alert('Tittle or description of taskt empty');
         }
         else{
-            console.log(tittle + " " + description);
+            const task = {
+                tittle,
+                description
+            }
 
-        }
-        e.preventDefault();
+            console.log(tittle + " " + description);
+            
+
+            if(localStorage.getItem('tasks') == null){//si la memoria esta vacia de tareas
+                let tasksList = [];
+                tasksList.push(task);
+                localStorage.setItem('tasks', JSON.stringify(tasksList));
+
+            }else{  
+                let tasks = JSON.parse(localStorage.getItem('tasks'));
+                tasks.push(task);
+
+                localStorage.setItem('tasks',JSON.stringify(tasks));
+                
+            }   
+            getTasks();
+        }   
+                e.preventDefault();
+                
+   }
+
+
+   function getTasks(){
+
+    let tasks = JSON.parse(localStorage.getItem('tasks'));
+    let tasksView = document.getElementById("tasks");
+
+    tasksView.innerHTML = '';
+
+    for(let i = 0; i < tasks.length; i++){
+    console.log(tasks[i]);       
+
+    }
+    
 
    }
+
+
+  
