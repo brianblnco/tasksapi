ac4495ac160507d584e1b45c5541042de0dcf303
Brian <brian@brian.com>
2021-09-20 23:52:28 +0200

    v1.2-OK

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ document.getElementById("formTask").addEventListener('submit', saveTask);
 
    alert("It works");
 
+   getTasks();
 
    function saveTask (e){ // capturar el event, e, capturar informacion cuando se ejecutta eñ evento sumbit.
         console.log(e);
@@ -33,27 +34,52 @@ document.getElementById("formTask").addEventListener('submit', saveTask);
 
                 localStorage.setItem('tasks',JSON.stringify(tasks));
                 
-            }   
+            }  
+
             getTasks();
+
+
+
+            console.log("El objeto se rellena ok: " + task.tittle + task.description);
+
+            
         }   
-                e.preventDefault();
+              
+        e.preventDefault();
                 
    }
 
 
+
    function getTasks(){
 
+    console.log("Entro en metodo getTasks()");
     let tasks = JSON.parse(localStorage.getItem('tasks'));
     let tasksView = document.getElementById("tasks");
 
+   //console.log(tasks);
     tasksView.innerHTML = '';
 
-    for(let i = 0; i < tasks.length; i++){
-    console.log(tasks[i]);       
-
+    if(tasks!=null){
+        
+        for(let i = 0; i < tasks.length; i++){
+           // console.log(tasks[i]);       
+                let tittle = tasks[i].tittle;
+                let description = tasks[i].description;
+        
+                tasksView.innerHTML += `<div class="card mb-3">
+                    <div class="card-body">
+                        <p>${tittle} - ${description}</p>
+                        <a class="btn btn-danger">
+                        Delete
+                        </a>
+                    </div>
+                </div>`
+            }
+        
+    
     }
     
-
    }
 
 
